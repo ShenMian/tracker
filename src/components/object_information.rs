790f0a0bb4f856878b19eab713c12702ad373cb7
ShenMian <sms_school@outlook.com>
2024-11-26 19:19:30 +0000

    feat: initial commit

---
+++ src/components/object_information.rs
@@ -0,0 +1,134 @@
+use anyhow::Result;
+use chrono::Utc;
+use ratatui::{
+    layout::{Margin, Rect},
+    style::{palette::tailwind, Style, Stylize},
+    text::Text,
+    widgets::{Block, Cell, Paragraph, Row, Table, Wrap},
+    Frame,
+};
+use reverse_geocoder::ReverseGeocoder;
+
+use crate::app::App;
+
+use super::Component;
+
+pub struct ObjectInformation {
+    geocoder: ReverseGeocoder,
+}
+
+impl Default for ObjectInformation {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ObjectInformation {
+    pub fn new() -> Self {
+        Self {
+            geocoder: ReverseGeocoder::new(),
+        }
+    }
+}
+
+impl Component for ObjectInformation {
+    fn render(&self, app: &App, frame: &mut Frame, area: Rect) -> Result<()> {
+        let block = Block::bordered().title("Object information".blue());
+        if let Some(index) = app.track_map.selected_object {
+            let object = &app.satellites.objects[index];
+            let state = object.predict(Utc::now()).unwrap();
+
+            let result = self.geocoder.search((state.latitude(), state.longitude()));
+            let city = result.record.name.clone();
+            let country = isocountry::CountryCode::for_alpha2(&result.record.cc)
+                .unwrap()
+                .name();
+
+            let items: Vec<(&str, String)> = vec![
+                ("Name", object.name().clone()),
+                ("COSPAR ID", object.cospar_id().clone()),
+                ("NORAD ID", object.norad_id().to_string()),
+                ("Longitude", format!("{:.2}", state.longitude())),
+                ("Latitude", format!("{:.2}", state.latitude())),
+                ("Altitude", format!("{:.2} km", state.altitude())),
+                ("Speed", format!("{:.2} km/s", state.speed())),
+                ("Location", format!("{}, {}", city, country)),
+                ("Epoch", object.epoch().to_string()),
+                (
+                    "Period",
+                    format!(
+                        "{} hr {} min {} ({:.2} min)",
+                        object.orbital_period().num_hours(),
+                        object.orbital_period().num_minutes() % 60,
+                        object.orbital_period().num_seconds() % 60,
+                        object.orbital_period().num_seconds() as f64 / 60.0
+                    ),
+                ),
+            ];
+
+            let inner_area = area.inner(Margin::new(1, 1));
+
+            let rows = items.iter().enumerate().map(|(i, item)| {
+                let color = match i % 2 {
+                    0 => tailwind::SLATE.c950,
+                    _ => tailwind::SLATE.c900,
+                };
+
+                let (property, value) = item;
+
+                // Split the value into multiple lines if it's too long
+                // FIXME: Byte splitting may cause UTF-8 characters to be truncated
+                let value = value
+                    .as_bytes()
+                    .chunks(((inner_area.width - 1) / 2) as usize)
+                    .map(String::from_utf8_lossy)
+                    .map(|s| s.trim_start().to_string())
+                    .collect::<Vec<_>>()
+                    .join("\n");
+
+                // Calculate the height of the row based on the number of lines
+                let height = (property.chars().filter(|c| *c == '\n').count() as u16)
+                    .max(value.chars().filter(|c| *c == '\n').count() as u16)
+                    + 1;
+
+                Row::new([
+                    Cell::from(Text::from(property.to_string())),
+                    Cell::from(Text::from(value.to_string())),
+                ])
+                .style(Style::new().bg(color))
+                .height(height)
+            });
+
+            let table = Table::default().block(block).rows(rows);
+
+            frame.render_widget(table, area);
+        } else {
+            let paragraph = Paragraph::new("No object selected".dark_gray())
+                .block(block)
+                .centered()
+                .wrap(Wrap { trim: true });
+
+            frame.render_widget(paragraph, area);
+        }
+
+        Ok(())
+    }
+}
+
+#[allow(dead_code)]
+fn format_longitude(longitude: f64) -> String {
+    if longitude >= 0.0 {
+        format!("{:.5}°E", longitude)
+    } else {
+        format!("{:.5}°W", longitude.abs())
+    }
+}
+
+#[allow(dead_code)]
+fn format_latitude(latitude: f64) -> String {
+    if latitude >= 0.0 {
+        format!("{:.5}°N", latitude)
+    } else {
+        format!("{:.5}°S", latitude.abs())
+    }
+}
---
+++ src/object.rs
@@ -0,0 +1,111 @@
+use chrono::{DateTime, Utc};
+
+#[derive(Clone, Debug)]
+pub struct Object {
+    name: String,
+    cospar_id: String,
+    norad_id: u64,
+
+    epoch: DateTime<Utc>,
+
+    constants: sgp4::Constants,
+    mean_motion: f64,
+}
+
+impl Object {
+    pub fn from_elements(elements: sgp4::Elements) -> Self {
+        Self {
+            name: elements.object_name.as_ref().unwrap().clone(),
+            cospar_id: elements.international_designator.as_ref().unwrap().clone(),
+            norad_id: elements.norad_id,
+            epoch: DateTime::from_naive_utc_and_offset(elements.datetime, Utc),
+            constants: sgp4::Constants::from_elements(&elements).unwrap(),
+            mean_motion: elements.mean_motion,
+        }
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn cospar_id(&self) -> &String {
+        &self.cospar_id
+    }
+
+    pub fn norad_id(&self) -> u64 {
+        self.norad_id
+    }
+
+    pub fn epoch(&self) -> DateTime<Utc> {
+        self.epoch
+    }
+
+    pub fn predict(&self, time: DateTime<Utc>) -> Result<State, sgp4::Error> {
+        let time_since_epoch = time - self.epoch;
+        let prediction = self.constants.propagate(sgp4::MinutesSinceEpoch(
+            time_since_epoch.num_seconds() as f64 / 60.0,
+        ))?;
+        let [lon, lat, alt] = ecef_to_geodetic(prediction.position);
+        assert!((-90.0..=90.0).contains(&lat));
+        assert!((-180.0..=180.0).contains(&lon));
+        Ok(State {
+            position: [lon, lat, alt],
+            velocity: prediction.velocity,
+        })
+    }
+
+    pub fn orbital_period(&self) -> chrono::Duration {
+        const SECONDS_PER_DAY: f64 = 24.0 * 60.0 * 60.0;
+        chrono::Duration::seconds((SECONDS_PER_DAY / self.mean_motion) as i64)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct State {
+    pub position: [f64; 3],
+    pub velocity: [f64; 3],
+}
+
+impl State {
+    pub fn latitude(&self) -> f64 {
+        self.position[1]
+    }
+
+    pub fn longitude(&self) -> f64 {
+        self.position[0]
+    }
+
+    pub fn altitude(&self) -> f64 {
+        self.position[2]
+    }
+
+    pub fn speed(&self) -> f64 {
+        (self.velocity[0].powi(2) + self.velocity[1].powi(2) + self.velocity[2].powi(2)).sqrt()
+    }
+}
+
+/// Converts ECEF coordinates to geodetic coordinates.
+fn ecef_to_geodetic(position: [f64; 3]) -> [f64; 3] {
+    let [x, y, z] = position;
+
+    let a = 6378.137; // Earth's semi-major axis (km)
+    let e2 = 0.00669437999014; // Square of Earth's ellipsoid eccentricity
+
+    let b = (x * x + y * y).sqrt();
+    let mut lat = (z / b).atan(); // Initial latitude
+
+    // Iterate to calculate latitude
+    for _ in 0..5 {
+        let sin_lat = lat.sin();
+        let n = a / (1.0 - e2 * sin_lat * sin_lat).sqrt();
+        lat = ((z + e2 * n * sin_lat) / b).atan();
+    }
+
+    let lon = y.atan2(x);
+
+    // Calculate altitude
+    let n = a / (1.0 - e2 * lat.sin().powi(2)).sqrt();
+    let alt = b / lat.cos() - n;
+
+    [lon.to_degrees(), lat.to_degrees(), alt]
+}
